e3a7eb5a5fdedbebf9a8e012fe28164c9f1b234a
cortex05 <havinginitiative@gmail.com>
2024-01-10 20:16:32 -0600

    feat: Mongoose and Subject schema

---
+++ backend/models/subjectModel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+
+const SubjectSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name of subject please.']
+    },
+    units: [
+      {
+        id: {
+          type: Number,
+          required: [true, 'Match index']
+        },
+        name: {
+          type: String,
+          required: [true, 'What unit number? ID plus 1']
+        },
+        explanations: [
+          {
+            id: {
+              type: Number,
+              required: [true, 'Match Index']
+            },
+            title: {
+              type: String,
+              required: [true, 'Gist of it']
+            },
+            body: {
+              type: String,
+              required: [true, 'Explaon what this is and why it\'s important']
+            }
+          }
+        ],
+        links: [
+          {
+            type: {
+              type: String,
+              required: [true, 'To video or document?']
+            },
+            id: {
+              type: String,
+              required: [true, 'Match index']
+            },
+            url: {
+              type: String,
+              required: [true, 'Where is it?']
+            }
+          }
+        ]
+      }
+    ]
+  }, {
+    timestamps: true
+  }
+)
+
+module.exports = mongoose.model('Subject', SubjectSchema)
